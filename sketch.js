098459a3f4605318fa9753d7b75d3f289a066aa2
Headary <adam@krska.com>
2018-07-31 01:30:14 +0200

    Testing game divs

--- sketch.js
+++ sketch.js
@@ -1,14 +1,13 @@
 
 function setup() {
   noCanvas();
-  var container = select('#grid_container');
   let divs = [];
   for(let i = 0; i < 5; i++) {
     let div = createDiv();
     div.id("item_container" + i);
     div.class("item_container");
     divs[i] = div;
-    div.parent(container);
+    div.parent("grid_container");
 
     let text = createP("Click!");
     text.class("game_names");
@@ -17,7 +16,5 @@ function setup() {
     let button = createButton("Play Game!");
     button.class("buttons");
     button.parent("item_container" + i);
-    
-    console.log("Creating item");
   }
 }
