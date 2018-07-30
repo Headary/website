6b39b8f01508706d8c95bd695cc606719aaa3d8b
Headary <adam@krska.com>
2018-07-30 23:31:18 +0200

    Testing .js

---
+++ Ball.js
@@ -0,0 +1,76 @@
+class Ball {
+  constructor() {
+    this.r = 12;
+    this.speed = 7;
+    this.reset();
+  }
+
+  show() {
+    noStroke();
+    fill(255);
+    ellipse(this.x, this.y, this.r * 2, this.r * 2);
+  }
+
+  update() {
+    this.x += this.xspeed;
+    this.y += this.yspeed;
+  }
+
+  edges() {
+    if (this.y - this.r < 0) {
+      this.yspeed *= -1;
+      this.y = this.r;
+    }
+
+    if (this.y + this.r > height) {
+      this.yspeed *= -1;
+      this.y = height - this.r;
+    }
+
+    if (this.x < 0) {
+      this.reset();
+      ding.play();
+      rscore++;
+    }
+
+    if (this.x > width) {
+      this.reset();
+      ding.play();
+      lscore++;
+    }
+  }
+
+  reset() {
+    this.x = width / 2;
+    this.y = height / 2;
+    let a = random(-PI / 4, PI / 4);
+    this.xspeed = this.speed * cos(a);
+    this.yspeed = this.speed * sin(a);
+    if (random(1) > 0.5) this.xspeed *= -1;
+  }
+
+  checkPaddleLeft(p) {
+    if (this.y < p.y + p.h / 2 && this.y > p.y - p.h / 2 && this.x - this.r < p.x + p.w / 2) {
+      if (this.x > p.x) {
+        this.bouncePaddle(p, 1);
+        this.x = p.x + p.w / 2 + this.r;
+      }
+    }
+  }
+
+  checkPaddleRight(p) {
+    if (this.y < p.y + p.h / 2 && this.y > p.y - p.h / 2 && this.x + this.r > p.x - p.w / 2) {
+      if (this.x < p.x) {
+        this.bouncePaddle(p, -1);
+        this.x = p.x - p.w / 2 - this.r;
+      }
+    }
+  }
+
+  bouncePaddle(p, dir) {
+    let diff = this.y - (p.y - p.h/2);
+    let angle = map(diff, 0, p.h, -PI/4, PI/4);
+    this.xspeed = this.speed * cos(angle) * dir;
+    this.yspeed = this.speed * sin(angle);
+  }
+}
---
+++ sketch.js
@@ -0,0 +1,54 @@
+
+let ball;
+let left, right;
+
+var lscore, rscore;
+
+function preload () {
+	 soundFormats('mp3', 'ogg');
+	 ding = loadSound('assets/ding.mp3');
+}
+
+function setup() {
+  createCanvas(800, 600);
+  ball = new Ball();
+  left = new Paddle(true);
+  right = new Paddle(false);
+	lscore = 0;
+	rscore = 0;
+	ding.setVolume(0.2);
+}
+
+function draw() {
+  background(51);
+
+	ball.checkPaddleLeft(left);
+	ball.checkPaddleRight(right);
+
+  left.update();
+  left.show();
+  right.update();
+  right.show();
+
+  ball.show();
+  ball.update();
+  ball.edges();
+
+	fill(255);
+  textSize(32);
+  text("" + lscore, 32, 40);
+  text("" + rscore, width - 64, 40);
+}
+
+function keyPressed() {
+  if (key == 'w' || key == 'W') left.move(-10);
+  else if (key == 's' || key == 'S') left.move(10);
+
+  if (keyCode == UP_ARROW) right.move(-10);
+  else if (keyCode == DOWN_ARROW) right.move(10);
+}
+
+function keyReleased() {
+  if (key == 'w' || key == 's' || key == 'W' || key == 'S') left.move(0);
+  if (keyCode == UP_ARROW || keyCode == DOWN_ARROW) right.move(0);
+}
