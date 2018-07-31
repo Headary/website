2a7e193c0c89ca66f2dbe27ba24599e02d3695b3
Headary <adam@krska.com>
2018-07-31 13:32:20 +0200

    Created divs and json file + divs generation

--- sketch.js
+++ sketch.js
@@ -1,20 +1,31 @@
+var data;
+
+function preload() {
+  data = loadJSON("games.json");
+}
 
 function setup() {
   noCanvas();
-  let divs = [];
-  for(let i = 0; i < 5; i++) {
+  for (let i = 0; i < data.games.length; i++) {
+    let game = data.games[i];
+
     let div = createDiv();
-    div.id("item_container" + i);
+    div.id("item_container_" + i);
     div.class("item_container");
-    divs[i] = div;
     div.parent("grid_container");
+    div.mousePressed(function () {divPressed(i);})
 
-    let text = createP("Click!");
-    text.class("game_names");
-    text.parent("item_container" + i);
+    let name = createP(game.name);
+    name.class("game_name");
+    name.parent("item_container_" + i);
 
-    let button = createButton("Play Game!");
-    button.class("buttons");
-    button.parent("item_container" + i);
+    let desc = createP(game.description);
+    desc.class("game_desc");
+    desc.parent("item_container_" + i);
   }
+}
+
+function divPressed(index) {
+  let folder = data.games[index].folder;
+  window.open("https://headary.github.io/website/" + folder);
 }
